d00b9ff92b2e3d7fcdfef47bbc5b6a8d7f455386
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-10-24 18:09:21 -0400

    randomized cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Card } from './Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [cardArray, setcardArray] = useState([])
+
+  //create random array
+
+  function shuffle() {
+    let array = [0, 1, 2, 3, 4, 5]
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    
+    return array
+  }
+
+ let randomArray = shuffle()
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      <Card />
+   
     </>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,80 @@
+import map from './assets/map.png';
+import pizza1 from './assets/pizza1.jpeg';
+import pizza from './assets/pizza.jpeg';
+import pizza2 from './assets/pizza2.jpeg';
+import pizza3 from './assets/pizza3.jpeg';
+import pizza4 from './assets/pizza4.jpeg';
+
+function Card(props) {
+
+    const {
+        number,
+
+    } = props;
+
+    let picArray = [map, pizza1, pizza, pizza2, pizza3, pizza4]
+
+    //create random array
+
+  function shuffle() {
+    let array = [0, 1, 2, 3, 4, 5]
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    
+    return array
+  }
+
+ let randomArray = shuffle()
+
+return (
+<>
+<div className="cardContainer">
+<div id={randomArray[0]} className="card">
+    <p>{randomArray[0]}</p>
+    <img className="img" src={picArray[randomArray[0]]}></img>
+    </div>
+
+    <div id={randomArray[1]} className="card">
+    <p>{randomArray[1]}</p>
+    <img className="img" src={picArray[randomArray[1]]}></img>
+    </div>
+
+    <div id={randomArray[2]} className="card">
+    <p>{randomArray[2]}</p>
+    <img className="img" src={picArray[randomArray[2]]}></img>
+    </div>
+
+    <div id={randomArray[3]} className="card">
+    <p>{randomArray[3]}</p>
+    <img className="img" src={picArray[randomArray[3]]}></img>
+    </div>
+
+    <div id={randomArray[4]} className="card">
+    <p>{randomArray[4]}</p>
+    <img className="img" src={picArray[randomArray[4]]}></img>
+    </div>
+
+    <div id={randomArray[5]} className="card">
+    <p>{randomArray[5]}</p>
+    <img className="img" src={picArray[randomArray[5]]}></img>
+    </div>
+</div>
+</>
+
+)
+    
+}
+
+
+export { Card };
